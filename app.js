50c9c31b2583c0d11407f314967f383d7ddc7958
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-10-15 18:06:45 +0700

    Tạo cấu trúc thư mục MVC chuẩn

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+app.get('/nguyen-van-thinh', (req, res) => {
+    res.send('Tôi là Thịnh')
+})
