73e67a84ff3f05d46a2952caf2ecbb4903c9aecd
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-10-22 17:51:40 +0700

    Cài đặt PUG template engine

--- src/app.js
+++ src/app.js
@@ -1,23 +1,37 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
-
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 import connectDB from './config/database.js';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+// set Pug as template engine
+app.set('view engine', 'pug');
+
+
+// set views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// static file public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Product Management API',
-        status: 'OK',
-        database: 'Connected'
-    });
+    res.render('pages/client/Home/index', { title: 'Home' });
 });
+
 
 export default app;
